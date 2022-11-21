42d76374ac8fb4b4ea0dcc45b207974953aef597
saurabh2816 <saurabh2816#>
2022-11-21 15:57:28 -0800

    new repo old work :)

---
+++ README.md
@@ -0,0 +1,3 @@
+# Opensourceflix
+
+The project aims to allow users to watch open index links on Google in a netflix-like layout. It does so by trying to extract the metadata from the link name and fetches the results from IMDB api. 
---
+++ src/app/model/Movie.ts
@@ -0,0 +1,31 @@
+import { Rating } from "./Rating"
+
+export interface Movie {
+    id: number,
+    srtLink: string,
+    link: string,
+    title: string ,
+    year: string ,
+    rated: string ,
+    released: string ,
+    runtime: string ,
+    genre: string ,
+    director: string ,
+    writer: string ,
+    actors: string ,
+    plot: string ,
+    language: string ,
+    country: string ,
+    awards: string ,
+    poster: string ,
+    metascore: string,
+    imdbRating: string,
+    imdbVotes: string,
+    imdbId: string,
+    type: string,
+    dvd: string,
+    boxOffice: string,
+    production: string,
+    website: string,
+    response: string
+}
